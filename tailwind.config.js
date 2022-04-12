c8a681a0da4d2b836bb2a6ca49c58a58fa3af48c
musayuksel <musayuxel@gmail.com>
2022-04-12 03:11:41 +0100

    Style:Add break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "750px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "768px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1024px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
