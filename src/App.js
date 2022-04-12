271d2510c10a3b70e1d1dd3d4dc5f827d508c002
musayuksel <musayuxel@gmail.com>
2022-04-12 16:04:35 +0100

    Feat:Add test sign-in button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import { signInWithGoogle } from "./firebase-config";
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button
+        onClick={signInWithGoogle}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Sing-in with goole
+      </button>
     </div>
   );
 }
