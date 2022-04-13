10966855532febdbc88e337270157c79a6cf4011
musayuksel <musayuxel@gmail.com>
2022-04-13 23:43:50 +0100

    Style:Add navbar tailwind classes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { signInWithGoogle } from "./firebase-config";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { createContext, useState } from "react";
 import About from "./components/About";
+import Blog from "./components/Blog";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
 export const AuthContext = createContext({ user: null });
 function App() {
   const [user, setUser] = useState(null);
@@ -10,16 +13,44 @@ function App() {
   return (
     <AuthContext.Provider value={user}>
       <Router>
-        <nav>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/about"}>About us</Link>
-          <Link to={"/blog"}>Blog</Link>
-          <Link to={"/pricing"}>Pricing</Link>
-          <Link to={"/contact"}>Contact</Link>
+        <nav className="flex border-b">
+          <Link
+            className="-mb-px mr-1 bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            to={"/"}
+          >
+            Home
+          </Link>
+          <Link
+            className="mr-1 bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            to={"/about"}
+          >
+            About us
+          </Link>
+          <Link
+            className="mr-1 bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            to={"/blog"}
+          >
+            Blog
+          </Link>
+          <Link
+            className="mr-1 bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            to={"/pricing"}
+          >
+            Pricing
+          </Link>
+          <Link
+            className="mr-1 bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            to={"/contact"}
+          >
+            Contact
+          </Link>
         </nav>
         <Routes>
           <Route path="/" element={<h1>HOME PAGE</h1>} />
           <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
       <div className="App container">
