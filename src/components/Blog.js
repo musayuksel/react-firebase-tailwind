95c9049ddb157e9e003fbf56596e4c61488c98ec
musayuksel <musayuxel@gmail.com>
2022-04-14 15:53:30 +0100

    feat:Add state and useEffect to Blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getUserData } from "../firebase-config";
 
-export default function Blog() {
+export default function Blog({ user }) {
+  const [userData, setUserData] = useState([]);
+  console.log({ userData });
+  useEffect(() => {
+    getUserData(setUserData, user);
+  }, []);
+  console.log({ userData }, ">>>>>>>>>>");
   return <div>Blog</div>;
 }
