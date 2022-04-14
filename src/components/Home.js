bbdebb3c4af55d3bf83b293c36a945b4e1846a39
musayuksel <musayuxel@gmail.com>
2022-04-14 13:09:55 +0100

    Feat:Add handleSubmit fuction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { signInWithGoogle } from "../firebase-config";
-
-export default function Home({ setUser }) {
+import { db, signInWithGoogle } from "../firebase-config";
+import { addDoc, collection } from "firebase/firestore";
+export default function Home({ user, setUser }) {
   const [name, setName] = useState("");
   const [income, setIncome] = useState(0);
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
+    const postCollectionRef = collection(db, "user_forms");
+    const result = await addDoc(postCollectionRef, {
+      name,
+      income: +income,
+      author: { email: user.email, id: user.uid },
+    });
+    console.log({ result });
   }
   return (
     <main className="App container">
@@ -16,43 +23,47 @@ export default function Home({ setUser }) {
       >
         Sign-in with Google
       </button>
-      <form onSubmit={handleSubmit} className="w-full max-w-lg">
-        <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="grid-name"
-        >
-          First Name
-        </label>
-        <input
-          onChange={(e) => setName(e.target.value)}
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          type="text"
-          value={name}
-          id="grid-name"
-          placeholder="Enter your name"
-        />
-        <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-        <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="gross-income"
-        >
-          Gross income
-        </label>
-        <input
-          onChange={(e) => setIncome(e.target.value)}
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          value={income}
-          type="number"
-          id="gross-income"
-          placeholder="Enter your gross income"
-        />
-        <button
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
+      {user && (
+        <form onSubmit={handleSubmit} className="w-full max-w-lg">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-name"
+          >
+            First Name
+          </label>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            type="text"
+            value={name}
+            id="grid-name"
+            placeholder="Enter your name"
+          />
+          <p className="text-red-500 text-xs italic">
+            Please fill out this field.
+          </p>
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="gross-income"
+          >
+            Gross income
+          </label>
+          <input
+            onChange={(e) => setIncome(e.target.value)}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            value={income}
+            type="number"
+            id="gross-income"
+            placeholder="Enter your gross income"
+          />
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      )}
     </main>
   );
 }
