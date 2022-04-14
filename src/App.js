809c1c3ef553e283c4bf141ef53d8b57c8761491
musayuksel <musayuxel@gmail.com>
2022-04-14 01:01:07 +0100

    Move login button to Home component

--- src/App.js
+++ src/App.js
@@ -4,34 +4,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createContext, useState } from "react";
 import About from "./components/About";
 import Blog from "./components/Blog";
+import Home from "./components/Home";
 import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 export const AuthContext = createContext({ user: null });
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   console.log({ user });
   return (
     <AuthContext.Provider value={user}>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>HOME PAGE</h1>} />
+          <Route path="/" element={<Home setUser={setUser} />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
-      <div className="App container">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <button
-          onClick={() => signInWithGoogle(setUser)}
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        >
-          Sing-in with Google
-        </button>
-      </div>
     </AuthContext.Provider>
   );
 }
