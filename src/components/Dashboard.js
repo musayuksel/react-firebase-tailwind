34886f8548226c3d16444d1b289b194d464c7c8f
musayuksel <musayuxel@gmail.com>
2022-04-15 03:35:16 +0100

    Feat:Add delete function for Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { getUserData } from "../firebase-config";
+import { getUserData, deleteUserData } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard({ user }) {
   const [userData, setUserData] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
+    if (!user) {
+      return navigate("/");
+    }
     getUserData(setUserData, user);
   }, [user]);
+  function handleDelete(documentId) {
+    deleteUserData(documentId).then(() => {
+      getUserData(setUserData, user);
+    });
+  }
   const dataCards = userData.map((user) => {
     return (
       <div
         key={user.documentId}
         className="max-w-sm rounded overflow-hidden shadow-lg"
       >
+        <button
+          onClick={() => handleDelete(user.documentId)}
+          className="bg-red-100  text-red-700 font-semibold py-2 px-4 border border-transparent hover:border-red-500 rounded"
+        >
+          X
+        </button>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{user.name}</div>
           <p className="text-gray-700 text-base">
