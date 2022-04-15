89109d1210414e5d241228b4cb0052289c638d46
musayuksel <musayuxel@gmail.com>
2022-04-15 02:52:24 +0100

    Feat:Add basic card for userDashboard

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,10 +3,27 @@ import { getUserData } from "../firebase-config";
 
 export default function Blog({ user }) {
   const [userData, setUserData] = useState([]);
-  console.log({ userData });
   useEffect(() => {
     getUserData(setUserData, user);
-  }, []);
-  console.log({ userData }, ">>>>>>>>>>");
-  return <div>Blog</div>;
+  }, [user]);
+  const dataCards = userData.map((user) => {
+    return (
+      <div
+        key={user.documentId}
+        className="max-w-sm rounded overflow-hidden shadow-lg"
+      >
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{user.name}</div>
+          <p className="text-gray-700 text-base">
+            {`User income is ${user.income} £/year`}
+          </p>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <>
+      <div className="container">{dataCards}</div>
+    </>
+  );
 }
